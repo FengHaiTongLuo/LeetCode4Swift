d3d9509525c620b1a57c6bd5f44ed57351202db7
风海铜锣 <88237308+FengHaiTongLuo@users.noreply.github.com>
2021-08-04 10:44:17 +0800

    Update 1. Two Sum.swift

--- 1. Two Sum.swift
+++ 1. Two Sum.swift
@@ -1,3 +1,4 @@
+// Solution 1.
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         for i in 0..<nums.count-1 {
@@ -8,5 +9,42 @@ class Solution {
             }
         }
         return [-1, -1]
+    }
+}
+
+// Solution 2.
+class Solution {
+    class Item {
+        var index = 0
+        var value = 0
+    }
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var items = nums.map { i -> Item in
+            let item = Item()
+            item.value = i
+            return item
+        }
+
+        for i in 0..<items.count {
+            items[i].index = i
+        }
+
+        items.sort { $0.value < $1.value }
+
+        var head = 0
+        var tail = items.count - 1
+
+        while head < tail {
+            let sum = items[head].value + items[tail].value
+            if sum > target {
+                tail -= 1
+            } else if sum < target {
+                head += 1
+            } else {
+                return [items[head].index, items[tail].index]
+            }
+        }
+
+        return []
     }
 }
