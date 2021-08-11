cd18a5f135da1923276f7d99445ea8c7ea57d4b2
风海铜锣 <88237308+FengHaiTongLuo@users.noreply.github.com>
2021-08-11 21:45:22 +0800

    Update 3. Longest Substring Without Repeating Characters.swift

--- 3. Longest Substring Without Repeating Characters.swift
+++ 3. Longest Substring Without Repeating Characters.swift
@@ -1,23 +1,32 @@
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        var iHead = 0
-        var iTail = 0
-        let cString = s.cString(using: .utf8)!
-        let count = cString.count - 1
-        var array = Array(repeating: -1, count: 10000)
-        var longest = 0
-        while iTail < count {
-            let key = Int(cString[iTail])
-            let si = array[key]
-            if si != -1 && si >= iHead {
-                iHead = si + 1
+        // 算法边界不包含空字符串，判定返回
+        guard !s.isEmpty else { return 0 }
+
+        let cs = s.cString(using: .ascii)!
+        let len = strlen(cs)
+
+        // 初始化i和j
+        var i = 0
+        var j = 1
+        
+        // cache存放遍历字符的位置
+        var cache: [Int] = Array(repeating: -1, count: 128)
+        var m = j - i
+        // cache保存着第一个字符的位置
+        cache[Int(cs[i])] = i
+        while j < len {
+            let k = cache[Int(cs[j])]
+            if k >= i {
+                // 当cache保存的字符位置在范围内，说明出现字符冲突
+                // 调整 i 让它往前跳
+                i = k + 1
             }
-            array[key] = iTail
-            if iTail - iHead + 1 > longest {
-                longest = iTail - iHead + 1
-            }
-            iTail += 1
+            cache[Int(cs[j])] = j
+            j += 1
+            // i被调整过后，或者没有冲突字符，新子串也是无重复子串
+            m = max(m, j-i)
         }
-        return longest
+        return m
     }
 }
